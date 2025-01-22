8b6920c74225c9ff9002d9f57a79f2775f70cfea
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2025-01-22 12:46:40 +0100

    Update bio.js

--- src/info/bio.js
+++ src/info/bio.js
@@ -1,8 +1,8 @@
 export const bio = {
     name: "Amolo Joseph",
-    title: "Full Stack Developer",
+    title: "Senior Software Developer",
     email: "joseph.amolo@cavidel.com",
     location: "Lagos, Nigeria",
-    shortDesc: "Full Stack Developer / Software Engineer , love hip hop music, Tech Guy.",
+    shortDesc: "Full Stack Developer / Software Engineer, love hip hop music, Tech Guy.",
     company: "Cavidel Limited",
 };
